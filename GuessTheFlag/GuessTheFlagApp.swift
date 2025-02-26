d23a57f33e422e30ee2ba35e336838a729b9c5b6
Kern Goretzky <kern.goretzky@gmail.com>
2025-02-26 09:51:07 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kern Goretzky on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
